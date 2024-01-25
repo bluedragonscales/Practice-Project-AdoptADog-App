099a8e679e26630e04c4b3de4743e315d518aeb5
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2024-01-24 21:16:45 -0800

    Created sessStor object for checkout total

--- scripts/adoptDogs.js
+++ scripts/adoptDogs.js
@@ -1,22 +1,28 @@
 // ADOPT DOGS JAVASCRIPT
 let cartTotal = document.getElementById('cartTotal');
 let buttons = document.getElementsByTagName('button');
-let total = 0;
+
+// Create the total variable and set it to either 0 or the total value from the "buttonPressed" function.
+let total = parseInt(sessionStorage.getItem('checkoutTotal'), 10) || 0;
 
 
 const buttonPressed = e => {
-//   console.log(e.target.id);
     if(e.target.id != 'checkout' && e.target.id != 'addPostBtn') {
         e.target.disabled = true;
         e.target.textContent = "Adopted!";
-
         total += 400;
-        sessionStorage.setItem('checkoutTotal', total);
+        // Save the the total into the sessStorage object.
+        window.sessionStorage.setItem('checkoutTotal', total);
         cartTotal.textContent = sessionStorage.getItem('checkoutTotal');
     };
-}
+};
 
 
 for (let button of buttons) {
   button.addEventListener("click", buttonPressed);
-}
+};
+
+
+window.addEventListener('load', (e) => {
+  cartTotal.textContent = parseInt(sessionStorage.getItem('checkoutTotal'), 10) || 0;
+});
